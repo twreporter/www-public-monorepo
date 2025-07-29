e9f0e1f135e4ea54f28051a99d3496a0425dc7f2
Lucien <liruchen0302@gmail.com>
2025-07-29 15:29:18 +0800

    feat(frontend): use tailwind and add logger

---
+++ packages/frontend/src/utils/logger.ts
@@ -0,0 +1,33 @@
+import { Logging } from '@google-cloud/logging'
+import pino from 'pino'
+
+const releaseBranch = process.env.NEXT_PUBLIC_RELEASE_BRANCH
+const logging = new Logging()
+const log = logging.log(`${releaseBranch}-congress-dashboard-frontend`)
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+const logger = pino({
+  level: 'info',
+  formatters: {
+    level: (label) => ({ severity: label.toUpperCase() }),
+  },
+  hooks: {
+    logMethod(inputArgs, method) {
+      const message = inputArgs[0]
+      const metadata = inputArgs[1] || {}
+      const entry = log.entry(
+        { resource: { type: 'global' } },
+        { message, ...metadata }
+      )
+
+      if (isProduction) {
+        log.write(entry) // Send logs to Google Cloud in production
+      }
+
+      method.apply(this, inputArgs) // Always log to console
+    },
+  },
+})
+
+export default logger
