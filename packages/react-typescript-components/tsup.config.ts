9d2a7c03ab91f542c736e434140251345f36db76
Lucien <liruchen0302@gmail.com>
2025-08-07 01:38:25 +0800

    feat(react-typescript-components): add tsup config

---
+++ packages/react-typescript-components/tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  format: ['cjs', 'esm'],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  outDir: 'lib',
+  external: ['react', 'react-dom'],
+  target: 'esnext',
+  treeshake: true,
+})
