ed53a44898cf22a5e67c97b93994744761e0054f
Aylie Chou <aylie_chou@twreporter.org>
2025-08-20 02:22:18 +0800

    feat(react-typescript-components): add `Title1`

---
+++ packages/react-typescript-components/src/title-bar/components/title1.tsx
@@ -0,0 +1,34 @@
+import clsx from 'clsx'
+import type { FC } from 'react'
+// components
+import { H2 } from '../../text/heading'
+import { P1 } from '../../text/paragraph'
+
+type Title1Props = {
+  title: string
+  subtitle?: string
+  className?: string
+}
+
+const Title1: FC<Title1Props> = ({
+  title,
+  subtitle = '',
+  className = '',
+}) => {
+  return (
+    <div className={clsx('flex justify-between flex-col', className)}>
+      <div className={clsx('flex items-baseline gap-[16px]')}>
+        <H2 className="text-gray-800" text={title} />
+        {subtitle ? <P1 className="text-gray-600" text={subtitle} /> : null}
+      </div>
+      <div
+        className={clsx(
+          'w-full h-[1px] bg-gray-800 mt-[8px]',
+          'desktop:mt-[16px]'
+        )}
+      />
+    </div>
+  )
+}
+
+export default Title1
--- packages/react-typescript-components/src/title-bar/index.ts
+++ packages/react-typescript-components/src/title-bar/index.ts
@@ -1,7 +1,9 @@
 import Title2 from './components/title2'
+import Title1 from './components/title1'
 
-export { Title2 }
+export { Title2, Title1 }
 
 export default {
   Title2,
+  Title1,
 }
