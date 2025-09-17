1a7e0f07b706679460b64bb32d312515f107dca7
Lucien <liruchen0302@gmail.com>
2025-09-17 19:57:57 +0800

    feat(react-typescript-components): header icons

---
+++ packages/react-typescript-components/src/header/components/top-row/icons.tsx
@@ -0,0 +1,24 @@
+import type React from 'react'
+// constants
+import { THEME, type Theme } from '../../../constants/theme'
+import {
+  RELEASE_BRANCH,
+  type ReleaseBranch,
+} from '../../../constants/release-branch'
+// icons
+import { Search, KidStar, Member } from '../../../icons'
+// button
+import { IconButton } from '../../../button'
+
+export const Icons: React.FC<{
+  releaseBranch: ReleaseBranch
+  theme: Theme
+}> = ({ releaseBranch = RELEASE_BRANCH.master, theme = THEME.normal }) => {
+  return (
+    <div className="ml-[24px] flex flex-row gap-[16px]">
+      <IconButton iconComponent={Search(releaseBranch)} theme={theme} />
+      <IconButton iconComponent={KidStar(releaseBranch)} theme={theme} />
+      <IconButton iconComponent={Member(releaseBranch)} theme={theme} />
+    </div>
+  )
+}
