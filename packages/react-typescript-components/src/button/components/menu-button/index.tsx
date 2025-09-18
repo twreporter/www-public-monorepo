76e31b27c52849d464c790053d501295143d5dae
Lucien <liruchen0302@gmail.com>
2025-09-18 23:04:51 +0800

    feat(react-typescript-components): menu button

---
+++ packages/react-typescript-components/src/button/components/menu-button/index.tsx
@@ -0,0 +1,45 @@
+import type React from 'react'
+import clsx from 'clsx'
+// P1
+import { P1 } from '../../../text/paragraph'
+import { WEIGHT, type Weight } from '../../../text/enum'
+
+type MenuButtonProps = {
+  text: string
+  color: string
+  fontWeight: Weight
+  hoverBgColor: string
+  activeBgColor: string
+  paddingLeft?: string
+  paddingRight?: string
+  className?: string
+}
+const MenuButton: React.FC<MenuButtonProps> & { FontWeight: typeof WEIGHT } = ({
+  text,
+  color,
+  fontWeight,
+  hoverBgColor,
+  activeBgColor,
+  paddingLeft = '',
+  paddingRight = '',
+  className = '',
+}) => {
+  return (
+    <div className={clsx('cusrsor-pointer', color, className)}>
+      <P1
+        text={text}
+        weight={fontWeight}
+        className={clsx(
+          'py-[8px]',
+          hoverBgColor,
+          activeBgColor,
+          paddingLeft,
+          paddingRight
+        )}
+      />
+    </div>
+  )
+}
+MenuButton.FontWeight = WEIGHT
+
+export default MenuButton
--- packages/react-typescript-components/src/button/index.ts
+++ packages/react-typescript-components/src/button/index.ts
@@ -1,6 +1,7 @@
 import TextButton from './components/text-button'
 import IconButton from './components/icon-button'
+import MenuButton from './components/menu-button'
 
-export { TextButton, IconButton }
+export { TextButton, IconButton, MenuButton }
 
-export default { TextButton, IconButton }
+export default { TextButton, IconButton, MenuButton }
