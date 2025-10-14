fb0716774346ea3dd38c067abbacf65bbe6f8d80
Lucien <liruchen0302@gmail.com>
2025-10-14 16:16:10 +0800

    feat(cms): update document title in CustomLogo component

--- packages/cms/admin/config.tsx
+++ packages/cms/admin/config.tsx
@@ -1,11 +1,16 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
-
+import { useEffect } from 'react'
 import type { AdminConfig } from '@keystone-6/core/types'
 import { jsx } from '@keystone-ui/core'
 import { CustomNavigation } from './components/CustomNavigation'
 
 function CustomLogo() {
+  useEffect(() => {
+    const currentTitle = document.title
+    const newTitle = currentTitle.replace('Keystone', '報導者 CMS')
+    if (newTitle !== currentTitle) document.title = newTitle
+  })
   return <h3>報導者 CMS</h3>
 }
 
