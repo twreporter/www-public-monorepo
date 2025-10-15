2dd9dca14236f1f24f988a4c634633d964c79494
Aylie Chou <aylie_chou@twreporter.org>
2025-10-15 22:14:45 +0800

    fix(frontend): apply fill-available on mobile only
    - autoprefixer would expend `width: fill-available` to `width: -moz-available` on chrome

--- packages/frontend/src/app/categories/[slug]/page.tsx
+++ packages/frontend/src/app/categories/[slug]/page.tsx
@@ -29,7 +29,7 @@ export default async function Page({
           },
         }}
       >
-        <div className="px-[24px] w-fill">
+        <div className="px-[24px] w-[fill-available] w-[-webkit-fill-available] tablet:w-auto">
           <CategoryPage
             slug={category.slug}
             name={category.name}
