a586b2e34384d88335c67772618824b0637c9b2a
Aylie Chou <aylie_chou@twreporter.org>
2025-11-02 17:33:52 +0800

    feat(cms): add member related lists

---
+++ packages/cms/lists/PostReadingTime.ts
@@ -0,0 +1,40 @@
+import { list } from '@keystone-6/core'
+import { integer, relationship } from '@keystone-6/core/fields'
+import {
+  allowAllRoles,
+} from './utils/access-control-list'
+
+const listConfigurations = list({
+  fields: {
+    user: relationship({
+      label: '使用者',
+      ref: 'User.postReadingTime',
+    }),
+    post: relationship({
+      label: '文章',
+      ref: 'Post',
+    }),
+    seconds: integer({
+      label: '秒數',
+      defaultValue: 0,
+      validation: { min: 0, isRequired: true },
+    })
+  },
+  access: {
+    operation: {
+      query: allowAllRoles(),
+      create: allowAllRoles(),
+      update: allowAllRoles(),
+      delete: allowAllRoles(),
+    },
+  },
+  ui: {
+    label: 'Post Reading Time',
+    listView: {
+      initialColumns: ['post', 'user', 'seconds'],
+      initialSort: { field: 'post', direction: 'DESC' },
+      pageSize: 50,
+    },
+  },
+})
+export default listConfigurations
