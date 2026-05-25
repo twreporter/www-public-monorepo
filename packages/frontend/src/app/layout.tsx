718ce23b68b3cd2326e65885d1a72e09daaedd4e
Lucien <liruchen0302@gmail.com>
2026-05-25 14:53:55 +0800

    feat(frontend): use head for script

--- packages/frontend/src/app/layout.tsx
+++ packages/frontend/src/app/layout.tsx
@@ -1,6 +1,5 @@
 import clsx from 'clsx'
 import type { Metadata } from 'next'
-import DOMPurify from 'isomorphic-dompurify'
 // components
 import Header from '@/components/header'
 import Footer from '@/components/footer'
@@ -21,20 +20,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <script
-        // biome-ignore lint/security/noDangerouslySetInnerHtml: add adobe fonts
-        dangerouslySetInnerHTML={{
-          __html: DOMPurify.sanitize(`(function(d) {
-            var config = {
-              kitId: 'vlk1qbe',
-              scriptTimeout: 3000,
-              async: true
-            },
-            h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-          })(document);
-        `),
-        }}
-      />
+      <head>
+        <script
+          // biome-ignore lint/security/noDangerouslySetInnerHtml: add adobe fonts
+          dangerouslySetInnerHTML={{
+            __html: `(function(d) {
+              var config = {
+                kitId: 'vlk1qbe',
+                scriptTimeout: 3000,
+                async: true
+              },
+              h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\\bwf-loading\\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+            })(document);
+          `,
+          }}
+        />
+      </head>
       <body className="tracking-[0.4px] leading-[1.4] antialiased">
         <Header />
         <main
