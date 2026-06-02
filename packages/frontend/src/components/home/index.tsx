839449dc177c779682e69d0d34a59f3d89df41fb
Lucien <liruchen0302@gmail.com>
2026-06-02 15:21:39 +0800

    feat(frontend): home page banner

--- packages/frontend/src/components/home/index.tsx
+++ packages/frontend/src/components/home/index.tsx
@@ -1,11 +1,20 @@
 'use client'
 import clsx from 'clsx'
+import { useContext } from 'react'
 // styles
 import { rwdGridOuterClass, rwdGridContainerClass } from '@/styles/layout'
 // components
 import { LatestSection } from '@/components/home/components/latest-section'
 import { EditorPicksSection } from '@/components/home/components/editor-picks-section'
 import { LatestTopicSection } from '@/components/home/components/latest-topic-section'
+import { HomePageBanner } from '@/components/home/components/banner'
+// context
+import { BaseContext } from '@/contexts'
+// banner config
+import {
+  getBannerConfigs,
+  BannerConfigTypes,
+} from '@/components/home/components/banner/config'
 // fake data
 import {
   fakeLatestSectionData,
@@ -15,12 +24,17 @@ import {
 
 // TODO: replace with real data and remove fake data after API is ready
 export const Home = () => {
+  const { releaseBranch } = useContext(BaseContext)
+  const configs = getBannerConfigs(releaseBranch)
   return (
     <div className={clsx(rwdGridOuterClass)}>
       <div className={clsx(rwdGridContainerClass)}>
         <LatestSection articles={fakeLatestSectionData} />
         <EditorPicksSection articles={fakeEditorPickSectionData} />
         <LatestTopicSection {...fakeLatestTopicSectionData} />
+        <HomePageBanner {...configs[BannerConfigTypes.DONATION]} />
+        <HomePageBanner {...configs[BannerConfigTypes.KIDS_REPORTER]} />
+        <HomePageBanner {...configs[BannerConfigTypes.PODCAST]} />
       </div>
     </div>
   )
