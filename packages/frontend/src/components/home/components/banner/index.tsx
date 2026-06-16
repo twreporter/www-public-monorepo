585399bd819039ed1ad6828880d928ddbdc086f7
Lucien <liruchen0302@gmail.com>
2026-06-16 17:30:31 +0800

    fix(frontend): home page banner and topic layout

--- packages/frontend/src/components/home/components/banner/index.tsx
+++ packages/frontend/src/components/home/components/banner/index.tsx
@@ -49,7 +49,9 @@ export const HomePageBanner: FC<HomePageBannerProps> = ({
             : undefined,
         }}
       >
-        <div className={clsx('w-full max-w-[280px] flex flex-col gap-[12px]')}>
+        <div
+          className={clsx('w-[calc(100vw-96px)] gap flex flex-col gap-[12px]')}
+        >
           <H2
             className={clsx('w-full text-center', textColor)}
             text={title}
@@ -57,7 +59,7 @@ export const HomePageBanner: FC<HomePageBannerProps> = ({
           />
           <P2 className={clsx('w-full', textColor)} text={description} />
         </div>
-        <div className="w-full max-w-[280px] flex justify-center">
+        <div className="w-[calc(100vw-96px)] gap flex justify-center">
           {buttonComponent}
         </div>
       </div>
